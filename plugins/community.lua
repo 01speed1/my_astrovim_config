5f1a60ab2434896222a38f4d66a59b4e15e3b8d2
Oscar Guzman <oscar.guzman@condorlabs.io>
2023-04-12 21:52:08 -0500

    Add copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,8 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.tokyonight" },
+  {
+    import = "astrocommunity.completion.copilot-lua-cmp"
+  }
+
 }
